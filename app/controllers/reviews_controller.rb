3d337e055f452aa366a34ac295b3980601653251
Vitaliy Suprunov <pambuvss@gmail.com>
2018-07-02 14:41:33 +0200

    Add init file for captcha

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,9 @@ class ReviewsController < ApplicationController
     @hotel = Hotel.find(params[:hotel_id])
     @review = @hotel.reviews.create(review_params)
     redirect_to hotel_path(@hotel)
+    @hotel.reviews.each do |review|
+  		
+	end
   end
  
   private
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.site_key  = '6LfVxGEUAAAAAIc-bLfwHMDQIe_7SDHVu2lkMuRS'
+  config.secret_key = '6LfVxGEUAAAAAAnCrcrq2LerCUhLholh1kn7QTE2'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
